ac19d09bc0756467924288a1233f64210deb7a86
Tristan Day <tristan.day.uk@gmail.com>
2023-11-23 11:55:38 +0000

    Test Cognito login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Amplify } from 'aws-amplify';
+import awsExports from "./aws-exports";
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(awsExports);
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <div>
+            <p>Welcome {user.username}</p>
+            <button onClick={signOut}>Sign out</button>
+          </div>
+        )}
+      </Authenticator>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
