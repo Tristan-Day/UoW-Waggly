34fe898145ee57d1b01f87647ab38a0829b15768
Tristan Day <tristan.day.uk@gmail.com>
2023-11-28 22:19:05 +0000

    Add user registration and update API route

--- amplify/backend/function/UserHandler/src/app.js
+++ amplify/backend/function/UserHandler/src/app.js
@@ -5,251 +5,194 @@ or in the "license" file accompanying this file. This file is distributed on an 
 See the License for the specific language governing permissions and limitations under the License.
 */
 
-const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
-const { DeleteCommand, DynamoDBDocumentClient, GetCommand, PutCommand, QueryCommand, ScanCommand } = require('@aws-sdk/lib-dynamodb');
+const {DynamoDBClient} = require('@aws-sdk/client-dynamodb');
+const {DynamoDBDocumentClient, GetCommand, PutCommand, QueryCommand, ScanCommand} = require('@aws-sdk/lib-dynamodb');
 
-const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
-
+const middleware = require('aws-serverless-express/middleware')
 const bodyParser = require('body-parser')
-const express = require('express')
 
-const client = new DynamoDBClient({ region: process.env.TABLE_REGION });
-const document = DynamoDBDocumentClient.from(client);
+// Dynamo DB Initalisation
+const dynamo = new DynamoDBClient({region : process.env.TABLE_REGION});
+const client = DynamoDBDocumentClient.from(dynamo);
 
 let tableName = "UserData";
-if (process.env.ENV && process.env.ENV !== "NONE") {
-  tableName = tableName + '-' + process.env.ENV;
+if (process.env.ENV && process.env.ENV !== "NONE")
+{
+    tableName = tableName + '-' + process.env.ENV;
 }
-
-const userIDPresent = true;
-
-const partitionKeyName = "IDENTIFIER";
-const partitionKeyType = "S";
-
-const sortKeyName = "FIRST_NAME";
-const sortKeyType = "S";
-
-const hasSortKey = sortKeyName !== "";
-
-const path = "/users";
-const UNAUTH = 'UNAUTH';
-
-const hashKeyPath = '/:' + partitionKeyName;
-const sortKeyPath = hasSortKey ? '/:' + sortKeyName : '';
 
 // Declare a new Express Application
+const express = require('express')
 const app = express()
+
 app.use(bodyParser.json())
-app.use(awsServerlessExpressMiddleware.eventContext())
+app.use(middleware.eventContext())
 
 // Enable CORS for all methods
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "*")
-  next()
+app.use(function(request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*")
+    response.header("Access-Control-Allow-Headers", "*")
+    next()
 });
 
-// Convert URL string param to expected Type
-const convertUrlType = (param, type) => {
-  switch (type) {
-    case "N":
-      return Number.parseInt(param);
-    default:
-      return param;
-  }
+function getMissingFields(source, requiredFields)
+{
+    var missingFields = [];
+
+    for (const field of requiredFields)
+    {
+        if (!(field in source))
+        {
+            missingFields.push(field);
+        }
+    }
+
+    return missingFields;
 }
 
-/************************************
-* HTTP Get method to list objects *
-************************************/
+const PATH = "/users";
 
-app.get(path, async function (req, res) {
-  var params = {
-    TableName: tableName,
-    Select: 'ALL_ATTRIBUTES',
-  };
+/***************************************************
+ * HTTP GET - Retreives a user based on Cognito ID *
+ ***************************************************/
 
-  try {
-    const data = await document.send(new ScanCommand(params));
-    res.json(data.Items);
-  } catch (err) {
-    res.statusCode = 500;
-    res.json({ error: 'Could not load items: ' + err.message });
-  }
-});
+// app.get(PATH + "/:IDENTIFIER", async function(req, res) {
+//     const condition = {};
+//     condition['IDENTIFIER'] = {
+//         ComparisonOperator : 'EQ'
+//     }
 
-/************************************
- * HTTP Get method to query objects *
- ************************************/
+//     if (req.apiGateway)
+//     {
+//         condition['IDENTIFIER']['AttributeValueList'] = [ req.apiGateway.event.requestContext.identity.cognitoIdentityId || "UNAUTH" ];
+//     }
+//     else
+//     {
+//         try
+//         {
+//             condition['IDENTIFIER']['AttributeValueList'] = [ convertUrlType(req.params['IDENTIFIER'], partitionKeyType) ];
+//         }
+//         catch (err)
+//         {
+//             res.statusCode = 500;
+//             res.json({error : 'Wrong column type ' + err});
+//         }
+//     }
 
-app.get(path + hashKeyPath, async function (req, res) {
-  const condition = {}
-  condition[partitionKeyName] = {
-    ComparisonOperator: 'EQ'
-  }
+//     let queryParams = {
+//         TableName : tableName,
+//         KeyConditions : condition
+//     }
 
-  if (userIDPresent && req.apiGateway) {
-    condition[partitionKeyName]['AttributeValueList'] = [req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH];
-  } else {
-    try {
-      condition[partitionKeyName]['AttributeValueList'] = [convertUrlType(req.params[partitionKeyName], partitionKeyType)];
-    } catch (err) {
-      res.statusCode = 500;
-      res.json({ error: 'Wrong column type ' + err });
+//     try
+//     {
+//         const data = await document.send(new QueryCommand(queryParams));
+//         res.json(data.Items);
+//     }
+//     catch (err)
+//     {
+//         res.statusCode = 500;
+//         res.json({error : 'Could not load items: ' + err.error});
+//     }
+// });
+
+/*************************************
+ * HTTP put method for insert object *
+ *************************************/
+
+// app.put(PATH, async function(req, res) {
+//     req.body['COGNITO_ID'] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || "UNAUTH";
+
+//     let putItemParams = {
+//         TableName : tableName,
+//         Item : req.body
+//     }
+//     try
+//     {
+//         let data = await document.send(new PutCommand(putItemParams));
+//         res.json({success : 'put call succeed!', url : req.url, data : data})
+//     }
+//     catch (err)
+//     {
+//         res.statusCode = 500;
+//         res.json({error : err, url : req.url, body : req.body});
+//     }
+// });
+
+/*************************************
+ * HTTP post method to update a user *
+ *************************************/
+
+app.post(PATH, async function(request, response)
+{
+    Object.keys(request.body).forEach(key => {
+        request.body[key].toUpperCase();
+    });
+
+    if (!(request.body['TYPE']))
+    {
+        response.status = 400;
+        response.json({error : "Missing required field 'TYPE'"});
+        return;
     }
-  }
 
-  let queryParams = {
-    TableName: tableName,
-    KeyConditions: condition
-  }
+    var requiredFields;
 
-  try {
-    const data = await document.send(new QueryCommand(queryParams));
-    res.json(data.Items);
-  } catch (err) {
-    res.statusCode = 500;
-    res.json({ error: 'Could not load items: ' + err.message });
-  }
-});
+    switch (request.body['TYPE'])
+    {
+    case 'owner':
+        requiredFields = [ 'FIRST_NAME', 'LAST_NAME' ];
+        break;
 
-/*****************************************
- * HTTP Get method for get single object *
- *****************************************/
+    case 'walker':
+        requiredFields = [ 'FIRST_NAME', 'LAST_NAME', 'REGION', 'POSTCODE', 'DESCRIPTION', 'RATE' ];
+        break;
 
-app.get(path + '/object' + hashKeyPath + sortKeyPath, async function (req, res) {
-  const params = {};
-  if (userIDPresent && req.apiGateway) {
-    params[partitionKeyName] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  } else {
-    params[partitionKeyName] = req.params[partitionKeyName];
-    try {
-      params[partitionKeyName] = convertUrlType(req.params[partitionKeyName], partitionKeyType);
-    } catch (err) {
-      res.statusCode = 500;
-      res.json({ error: 'Wrong column type ' + err });
+    default:
+        response.status = 400;
+        response.json({error : `'${request.body['TYPE']}' is not a valid user type`});
+        return;
     }
-  }
-  if (hasSortKey) {
-    try {
-      params[sortKeyName] = convertUrlType(req.params[sortKeyName], sortKeyType);
-    } catch (err) {
-      res.statusCode = 500;
-      res.json({ error: 'Wrong column type ' + err });
+
+    const missingFields = getMissingFields(request.body, requiredFields);
+    if (missingFields.length != 0)
+    {
+        response.status = 400
+        response.json({error : "Missing required field(s)", missingFields})
+        return;
     }
-  }
 
-  let getItemParams = {
-    TableName: tableName,
-    Key: params
-  }
+    // Filter request fields
+    var user = {};
 
-  try {
-    const data = await document.send(new GetCommand(getItemParams));
-    if (data.Item) {
-      res.json(data.Item);
-    } else {
-      res.json(data);
+    Object.keys(request.body).forEach(key => {
+        if (requiredFields.includes(key))
+        {
+            user[key] = request.body[key];
+        }
+    });
+
+    // Set identifier field from Cognito Access Key
+    user['IDENTIFIER'] = request.apiGateway.event.requestContext.identity.accessKey || "UNAUTH";
+
+    try
+    {
+        const parameters = {TableName : tableName, Item : user};
+        await client.send(new PutCommand(parameters));
+
+        response.status = 200;
+        response.json({message : "User Sucessfully Updated"});
     }
-  } catch (err) {
-    res.statusCode = 500;
-    res.json({ error: 'Could not load items: ' + err.message });
-  }
-});
-
-
-/************************************
-* HTTP put method for insert object *
-*************************************/
-
-app.put(path, async function (req, res) {
-
-  if (userIDPresent) {
-    req.body['userId'] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  }
-
-  let putItemParams = {
-    TableName: tableName,
-    Item: req.body
-  }
-  try {
-    let data = await document.send(new PutCommand(putItemParams));
-    res.json({ success: 'put call succeed!', url: req.url, data: data })
-  } catch (err) {
-    res.statusCode = 500;
-    res.json({ error: err, url: req.url, body: req.body });
-  }
-});
-
-/************************************
-* HTTP post method for insert object *
-*************************************/
-
-app.post(path, async function (req, res) {
-
-  if (userIDPresent) {
-    req.body['userId'] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  }
-
-  let putItemParams = {
-    TableName: tableName,
-    Item: req.body
-  }
-  try {
-    let data = await document.send(new PutCommand(putItemParams));
-    res.json({ success: 'post call succeed!', url: req.url, data: data })
-  } catch (err) {
-    res.statusCode = 500;
-    res.json({ error: err, url: req.url, body: req.body });
-  }
-});
-
-/**************************************
-* HTTP remove method to delete object *
-***************************************/
-
-app.delete(path + '/object' + hashKeyPath + sortKeyPath, async function (req, res) {
-  const params = {};
-  if (userIDPresent && req.apiGateway) {
-    params[partitionKeyName] = req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH;
-  } else {
-    params[partitionKeyName] = req.params[partitionKeyName];
-    try {
-      params[partitionKeyName] = convertUrlType(req.params[partitionKeyName], partitionKeyType);
-    } catch (err) {
-      res.statusCode = 500;
-      res.json({ error: 'Wrong column type ' + err });
+    catch (error)
+    {
+        response.statusCode = 500;
+        response.json({text : "Failed to Process Request", error : error});
     }
-  }
-  if (hasSortKey) {
-    try {
-      params[sortKeyName] = convertUrlType(req.params[sortKeyName], sortKeyType);
-    } catch (err) {
-      res.statusCode = 500;
-      res.json({ error: 'Wrong column type ' + err });
-    }
-  }
-
-  let removeItemParams = {
-    TableName: tableName,
-    Key: params
-  }
-
-  try {
-    let data = await document.send(new DeleteCommand(removeItemParams));
-    res.json({ url: req.url, data: data });
-  } catch (err) {
-    res.statusCode = 500;
-    res.json({ error: err, url: req.url });
-  }
 });
 
-app.listen(3000, function () {
-  console.log("App started")
+app.listen(3000, function()
+{
+    console.log("App started")
 });
-
-// Export the app object. When executing the application local this does nothing. However,
-// to port it to AWS Lambda we will create a wrapper around that will load the app from
-// this file
 
 module.exports = app
--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
+// clang-format off
+
 import logo from './logo.svg';
+
 import './App.css';
 
-import { Amplify, API } from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
+import { get, post, put } from 'aws-amplify/api';
+
 import awsExports from "./aws-exports";
 
 import { Authenticator } from '@aws-amplify/ui-react';
 import { withAuthenticator } from '@aws-amplify/ui-react';
+
 import '@aws-amplify/ui-react/styles.css';
 
-import { useEffect } from 'react';
 
 Amplify.configure(awsExports);
 
-function App() {
-  useEffect(() => {
-    API.get("UserService", )
-  }, [])
+async function createUser(identifier)
+{
+    try
+    {
+      const operation = post
+      ({
+        apiName: "UserService",
+        path: "/users",
+        options: 
+        {
+          body: {"TYPE" : "owner", "FIRST_NAME" : "Tristan", "LAST_NAME": "Day"}
+        }
+      });
 
+      const { body } = await operation.response;
+      console.log(await body.json());
+    }
+    catch (error)
+    {
+        console.error(error);
+    }
+}
+
+function App()
+{
   return (
     <div className="App">
       <Authenticator>
         {({ signOut, user }) => (
           <div>
-            <p>Welcome {user.username}</p>
+            <button onClick={() => {
+            createUser(user.userId)}}>Create User</button>
             <button onClick={signOut}>Sign out</button>
           </div>
         )}
