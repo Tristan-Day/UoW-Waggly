80cc6fc5ffd3f0c04e96873f2989e252cd870fd7
Tristan Day <tristan.day.uk@gmail.com>
2023-11-29 22:34:48 +0000

    Add list route for pets

--- amplify/backend/function/PetHandler/src/app.js
+++ amplify/backend/function/PetHandler/src/app.js
@@ -5,8 +5,8 @@ or in the "license" file accompanying this file. This file is distributed on an 
 See the License for the specific language governing permissions and limitations under the License.
 */
 
-const {DynamoDBClient} = require("@aws-sdk/client-dynamodb");
-const {DynamoDBDocumentClient, GetCommand, PutCommand, QueryCommand} = require("@aws-sdk/lib-dynamodb");
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, GetCommand, PutCommand, QueryCommand, DeleteCommand } = require("@aws-sdk/lib-dynamodb");
 
 const middleware = require("aws-serverless-express/middleware")
 const bodyParser = require("body-parser")
@@ -106,6 +106,37 @@ app.get(PATH + "/:NAME", async function(request, response)
   }
 })
 
+/******************************************************************
+ * HTTP GET - Retreives Pets associated with the given Cognito ID *
+ ******************************************************************/
+
+app.get(PATH, async function(request, response)
+{
+  const user = request.apiGateway.event.requestContext.identity.cognitoIdentityId || "UNAUTH"
+
+  const parameters = 
+  {
+    TableName: tableName,
+    IndexName: "UserIndex",
+    KeyConditionExpression: "USER_IDENTIFIER = :user",
+    ExpressionAttributeValues: {":user" : user}
+  }
+
+  // Query the Database
+  try
+  {
+    const data = await client.send(new QueryCommand(parameters))
+
+    response.status = 200;
+    response.json(data.Items);
+  }
+  catch (error)
+  {
+    response.statusCode = 500;
+    response.json({text : "Failed to Process Request", error : error});
+  }  
+})
+
 /**************************************
  * HTTP POST - Create or Update a Pet *
  **************************************/
@@ -157,6 +188,39 @@ app.post(PATH + "/:NAME", async function(request, response)
       response.json({text : "Failed to Process Request", error : error});
     }
 });
+
+/************************************************
+ * HTTP DELETE - Remove a Pet from the Database *
+ ************************************************/
+
+app.delete(PATH + "/:NAME", async function(request, response)
+{
+  const key = 
+  {
+    NAME: request.params.NAME, 
+    USER_IDENTIFIER: request.apiGateway.event.requestContext.identity.cognitoIdentityId || "UNAUTH"
+  }
+
+  const parameters = 
+  {
+    TableName: tableName,
+    Key: key
+  }
+
+  // Query the Database
+  try
+  {
+    await client.send(new DeleteCommand(parameters));
+
+    response.status = 200;
+    response.json({message : "Pet Sucessfully Removed"});
+  }
+  catch (error)
+  {
+    response.statusCode = 500;
+    response.json({text : "Failed to Process Request", error : error});
+  }
+})
 
 app.listen(3000, function() {
   console.log("App started")
